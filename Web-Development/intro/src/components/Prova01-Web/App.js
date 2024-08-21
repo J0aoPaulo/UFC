b1c4aaeaef429777dde92b23f6319b870afdcb09
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2024-08-21 08:54:08 -0300

    Questao 02 check

---
+++ Web-Development/intro/src/components/Prova01-Web/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Questao01A from "./components/Prova01-Web/Questao01"; // Importando o componente Questao01A
+import Questao02 from "./components/Prova01-Web/Questao02"; // Importando o componente Questao02
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Teste da Questao01</h1>
+      <Questao01A /> */}
+      <h1>Teste da Questao02</h1>
+      <Questao02 />
+    </div>
+  );
+}
+
+export default App;
--- Web-Development/intro/src/components/Prova01-Web/Questao02.jsx
+++ Web-Development/intro/src/components/Prova01-Web/Questao02.jsx
@@ -1,2 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
+const Questao02 = () => {
+  // Estado para alternar entre a imagem frontal e a traseira
+  const [isFront, setIsFront] = useState(true);
+
+  const frontImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+  const backImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png";
+
+  // Função para alternar a imagem exibida
+  const toggleImage = () => {
+    setIsFront(!isFront);
+  };
+
+  // Redenrização do componente
+  return (
+    <div>
+      <img src={isFront ? frontImage : backImage} alt="Pokemon" />
+      <button onClick={toggleImage}>Alternar Imagem</button>
+    </div>
+  );
+};
+
+export default Questao02;
